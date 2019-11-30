d1f9bdd1a7d33518d08bbfdefc940e5658d2f07c
garima1930 <54432231+garima1930@users.noreply.github.com>
2019-11-30 14:04:11 +0530

    Update pre-build-config.bat

--- Source/pre-build-config.bat
+++ Source/pre-build-config.bat
@@ -1,3 +1,3 @@
 SET PATH=%PATH%;C:\Users\HOME\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
-git pull
+git pull "https://github.com/garima1930/PyMicroservices.git"
 gcloud auth activate-service-account garru-780@pymicroservices.iam.gserviceaccount.com --key-file=C:\Users\HOME\Desktop\pymicroservices-193d4f972fd8.json --project=pymicroservices
