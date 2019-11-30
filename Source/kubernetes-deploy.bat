26e6906ea709022850c835ebfa5d839395dad42b
garima1930 <54432231+garima1930@users.noreply.github.com>
2019-11-30 13:01:28 +0530

    Update kubernetes-deploy.bat

--- Source/kubernetes-deploy.bat
+++ Source/kubernetes-deploy.bat
@@ -1,5 +1,5 @@
 SET PATH=%PATH%;C:\Users\HOME\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
-SET BUILD_NUMBER=2
+
 SET IMAGE_NAME=gcr.io/handle-books/gpymicro-image:%BUILD_NUMBER%
 cd C:\Users\HOME\Desktop\PyMicroservices-master
 kubectl set image deployment/py-gmicro py-gmicro=%IMAGE_NAME%
