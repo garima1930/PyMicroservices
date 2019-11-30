cead80ad683910e90f6bdc66f1b456c9c3c5745c
garima1930 <garima.pandita@st.niituniversity.in>
2019-11-30 12:54:00 +0530

    hello667575757

--- Source/docker-image-build.bat
+++ Source/docker-image-build.bat
@@ -1,4 +1,5 @@
 SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
+SET BUILD_NUMBER=3
 SET IMAGE_NAME=gcr.io/pymicroservices/gpymicro-image:%BUILD_NUMBER%
 cd C:\Users\HOME\Desktop\PyMicroservices-master\Source
 gcloud builds submit --tag %IMAGE_NAME% .
